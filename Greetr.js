fd748afdc6244c4e4cfb8bb648326e286e94f475
ashfaq <ashfaq.m@techsophy.com>
2018-12-03 16:49:28 +0530

    properties and Chainable codes

--- Greetr.js
+++ Greetr.js
@@ -2,11 +2,67 @@
 
   var Greetr = function (firstName, lastName, language) {
     return new Greetr.init({ firstName, lastName, language })
+  };
+  var supportedLanguages = ['en', 'es'];
+  var greetings = {
+    en:'Hello',
+    es:'Hola'
+  };
+
+  var formalGreetings = {
+    en:'Greetings',
+    es:'Saludos'
+  };
+ 
+  var logMessages = {
+    en:'LoggedIn',
+    es:'iniciar sesión'
   }
 
   Greetr.prototype = {
+    fullName: function(){
+      return `${this.firstName} ${this.lastName}`;
+    },
+    validate: function(){
+      if(!supportedLanguages.includes(this.language)){
+        throw new Error('unsupported language');
+      }
+    },
 
-  }
+    greeting: function(){
+      return `${greetings[this.language]} ${this.firstName} !`;
+    },
+
+    formalGreetings: function(){
+      return `${formalGreetings[this.language]} ${this.fullName()}`;
+    },
+    
+    greet: function(formal){
+      var msg;
+      if(formal){
+        msg = this.formalGreetings()
+      }else{
+        msg = this.greeting()
+      }
+      if(console){
+        console.log(msg);
+      }
+      return this;
+    },
+
+    log: function(){
+      if(console){
+        console.log(`${logMessages[this.language]}: ${this.fullName()}`);
+      }
+      return this;
+    },
+
+    setLanguage : function(newlanguage){
+      this.language = newlanguage;
+      this.validate();      
+      return this
+    }
+  };
 
   Greetr.init = function ({ firstName = '', lastName = '', language = 'en' }) {
     var self = this;
@@ -14,7 +70,7 @@
     self.lastName = lastName;
     self.language = language;
     return self
-  }
+  };
 
   Greetr.init.prototype = Greetr.prototype;
 
--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-var g = G$('firstname', 'lastname', 'language');
+var g = G$('ashfaq', 'ansari', 'en');
 
-console.log(g);
+g.greet();
+
+// g.setLanguage("SDSD"); // unsupported language
+
+g.greet().greet(); // chanable functions
+
+
+g.setLanguage('es');
+
+g.greet().log();
